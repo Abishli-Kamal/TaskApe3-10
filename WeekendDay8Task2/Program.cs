82d65c88a67577b6e31268472f7e73afe638c042
Abishli-Kamal <tu7ldxfzy@code.edu.az>
2022-05-14 01:37:43 +0400

    TskApe3

---
+++ WeekendDay8Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace WeekendDay8Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("Kamal",20,2000.50);
+            Employee employee1 = new Employee("Baxshali",21,800.20);
+            Employee employee2 = new Employee("Malik",19,1500.90);
+            Employee employee3 = new Employee("Islam", 22, 2200.10);
+
+            Deparment deparment = new Deparment("Bdu",4);
+
+            deparment.AddEmployee(employee);
+            deparment.AddEmployee(employee1);
+            deparment.AddEmployee(employee2);
+            deparment.AddEmployee(employee3);
+
+            foreach (var item in deparment.employees)
+            {
+                Console.WriteLine(item.ShowInfo());
+            }
+
+            Console.WriteLine("\nFoundById");
+            Console.WriteLine(deparment[2]);
+        }
+    }
+}
